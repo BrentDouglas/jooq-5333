79b7613409acf0ab5551e2bf68527a974f165754
Brent Douglas <brent.n.douglas@gmail.com>
2016-10-19 10:50:04 -0700

    Make UDT and data more complicated to illustrate nested quoting issues

--- src/main/resources/db/migration/V0__schema.sql
+++ src/main/resources/db/migration/V0__schema.sql
@@ -1,7 +1,9 @@
+set search_path to test,public;
+
 create type foo as (
   id int,
   name text,
-  otherThing text
+  json json
 );
 
 create type bar as (
@@ -9,14 +11,26 @@ create type bar as (
   foos foo[]
 );
 
+create type baz as (
+  id int,
+  foos foo[],
+  bars bar[]
+);
+
 create table t_foo (
   id serial primary key,
   name text not null,
-  other text
+  json json
 );
 create table t_bar (
   id serial primary key,
   foo_ids int[]
+);
+create table t_baz (
+  id serial primary key,
+  foo_ids int[],
+  foo_jsons json[],
+  bar_ids int[]
 );
 
 create or replace view v_bar as
@@ -33,9 +47,42 @@ create or replace view v_bar as
               row(
                   f.id,
                   f.name,
-                  f.other
+                  f.json
               )::foo
           )::foo[] as data
       from t_foo f
       where f.id = any(b.foo_ids)
   ) f on true;
+
+create or replace view v_baz as
+  select
+      b.id,
+      row(
+          b.id,
+          coalesce(f.data, '{}'),
+          coalesce(f.json, '{}'),
+          coalesce(r.data, '{}')
+      )::baz as data
+  from t_baz b
+  left join lateral (
+      select
+          f.json,
+          array_agg(
+              row(
+                  f.id,
+                  f.name,
+                  f.json
+              )::foo
+          )::foo[] as data
+      from t_foo f
+      where f.id = any(b.foo_ids)
+  ) f on true
+  left join lateral (
+      select
+          array_agg(
+                  v.data
+          )::bar[] as data
+      from t_bar f
+      left join v_bar v on f.id=v.id
+      where f.id = any(b.bar_ids)
+  ) r on true;
--- src/test/java/jooq5333/jOOQ5333Test.java
+++ src/test/java/jooq5333/jOOQ5333Test.java
@@ -1,8 +1,9 @@
 package jooq5333;
 
-import jooq5333.udt.records.BarRecord;
+import jooq5333.udt.records.BazRecord;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
+import org.jooq.exception.DataAccessException;
 import org.jooq.impl.DSL;
 import org.jooq.impl.DefaultConfiguration;
 import org.junit.Assert;
@@ -10,11 +11,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 import static jooq5333.Tables.T_BAR;
+import static jooq5333.Tables.T_BAZ;
 import static jooq5333.Tables.T_FOO;
-import static jooq5333.Tables.V_BAR;
+import static jooq5333.Tables.V_BAZ;
 
 /**
  * @author <a href="mailto:brent.douglas@ysura.com">Brent Douglas</a>
@@ -24,36 +27,60 @@ public class jOOQ5333Test extends Assert {
     DSLContext db;
 
     @Before
-    public void before() throws Exception{
+    public void before() throws Exception {
         db = DSL.using(new DefaultConfiguration()
                 .set(DriverManager.getConnection("jdbc:postgresql://localhost/jooq5333"))
                 .set(SQLDialect.POSTGRES_9_4));
+        db.truncate(T_BAZ).execute();
         db.truncate(T_BAR).execute();
         db.truncate(T_FOO).execute();
     }
 
     @Test
     public void testJOOQ5333() throws Exception {
-        db.batch(
-                db.insertInto(T_FOO)
-                        .set(T_FOO.ID, 1)
-                        .set(T_FOO.NAME, "First"),
-                db.insertInto(T_FOO)
-                        .set(T_FOO.ID, 2)
-                        .set(T_FOO.NAME, "Second"),
-                db.insertInto(T_BAR)
-                        .set(T_BAR.ID, 1)
-                        .set(T_BAR.FOO_IDS, new Integer[]{ 1, 2 })
-        ).execute();
+        try {
+            db.batch(
+                    db.insertInto(T_FOO)
+                            .set(T_FOO.ID, 1)
+                            .set(T_FOO.NAME, "\"First"),
+                    db.insertInto(T_FOO)
+                            .set(T_FOO.ID, 2)
+                            .set(T_FOO.NAME, "\"Quoted & , asdf")
+                            .set(T_FOO.JSON, "{\"foo\":9,\"bar\":[\"a\",\"b\"],\"c\":{\"a\":[{\"b\":5},{\"b\":5}]}}"),
+                    db.insertInto(T_FOO)
+                            .set(T_FOO.ID, 3)
+                            .set(T_FOO.NAME, "adf")
+                            .set(T_FOO.JSON,
+                                    "[{\"foo\":9,\"bar\":[\"a\",\"b\"],\"c\":{\"a\":[{\"b\":5},{\"b\":5}]}},{\"foo\":9,\"bar\":[\"a\",\"b\"],\"c\":{\"a\":[{\"b\":5},{\"b\":5}]}}]"),
+                    db.insertInto(T_FOO)
+                            .set(T_FOO.ID, 4)
+                            .set(T_FOO.NAME, "asdgf"),
+                    db.insertInto(T_BAR)
+                            .set(T_BAR.ID, 1)
+                            .set(T_BAR.FOO_IDS, new Integer[]{1, 2}),
+                    db.insertInto(T_BAR)
+                            .set(T_BAR.ID, 2)
+                            .set(T_BAR.FOO_IDS, new Integer[]{3, 4}),
+                    db.insertInto(T_BAZ)
+                            .set(T_BAZ.ID, 1)
+                            .set(T_BAZ.FOO_IDS, new Integer[]{1, 2, 3, 4})
+                            .set(T_BAZ.BAR_IDS, new Integer[]{1, 2})
+            ).execute();
 
-        final List<BarRecord> ret = db
-                .select(V_BAR.DATA)
-                .from(T_BAR)
-                .leftJoin(V_BAR)
-                .on(V_BAR.ID.eq(T_BAR.ID))
-                .fetchInto(BarRecord.class);
+            final List<BazRecord> ret = db
+                    .select(V_BAZ.DATA)
+                    .from(T_BAZ)
+                    .leftJoin(V_BAZ)
+                    .on(V_BAZ.ID.eq(T_BAZ.ID))
+                    .fetchInto(BazRecord.class);
 
-        assertEquals(0, ret.size());
+            assertEquals(1, ret.size());
+        } catch (final DataAccessException e) {
+            if (e.getCause() instanceof SQLException) {
+                e.addSuppressed(((SQLException) e.getCause()).getNextException());
+            }
+            throw e;
+        }
     }
 
 }
